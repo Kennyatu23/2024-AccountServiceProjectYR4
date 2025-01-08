c832fabbbfb023d7b53f4dec981b962f44b31c9c
G00394277 <G00394277@atu.ie>
2025-01-08 21:21:32 +0000

    vid demo

--- src/main/java/ie/atu/accountservice/PaymentServiceClient.java
+++ src/main/java/ie/atu/accountservice/PaymentServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "Payment-Service", url = "${feign.url2}")
+@FeignClient(name = "Payment-Service", url = "http://localhost:8082")
 public interface PaymentServiceClient {
     @PostMapping("balanceNotify")
     String getBalance(@RequestBody AccountDetails accountDetails);
